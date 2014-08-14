44caa7c788b595282f2d97236e98f0c5db1520c4
Laura Skelton <lauraeskelton@gmail.com>
2014-08-13 22:42:32 -0400

    First Commit

--- DogChat/ViewController.m
+++ DogChat/ViewController.m
@@ -8,8 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define streamURLString @"http://10.0.6.73:8080/plaincam.html"
 
+@interface ViewController ()
+@property (nonatomic, retain) IBOutlet UIWebView *webView;
+
+-(IBAction)giveTreat:(id)sender;
 @end
 
 @implementation ViewController
@@ -17,13 +21,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSURL *streamURL = [NSURL URLWithString:streamURLString];
+
+    [self.webView setFrame: self.view.bounds];
+    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:streamURL]];
+    
+    [self gotDogText:nil];
+    
 }
+
+-(IBAction)giveTreat:(id)sender
+{
+    // This will be hooked up to send a get request to the Raspberry Pi
+    // that will tell the servo to move and dispense a treat
+    NSLog(@"Treat!");
+}
+
+-(void)gotDogText:(id)sender
+{
+    // This will be called when the dog has pressed the force-sensitive resistor button
+    // on which we will be monitoring for updates,
+    // and will read as if the dog has texted us.
+    
+    // notify user of new DogChat message
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.alertBody = @"Message from Ada: \"Woof.\"";
+    notification.soundName = @"Default";
+    [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
+}
+
+-(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    [self.webView reload];
+}
+
 
 @end
---
+++ README.md
@@ -0,0 +1,18 @@
+DogChat
+===========
+
+Video chatting for dogs.
+
+Hardware
+------
+
+The DogChat hardware is built on top of a Raspberry Pi. The Pi starts a web server and uses an attached USB Webcam to create a video stream over a local port. It uses ngrok to allow access to itself from outside of the local network. GET requests to the Raspberry Pi control the servo motor, which is used to dispense a treat to the dog, and allows a viewer to access the video feed. There is a force-sensitive resistor used as a button the dog can press to send a message to the user. (Currently in progress)
+
+iOS App
+------
+
+The DogChat iOS app is like FaceTime for dogs. There is a live video stream displayed fullscreen, with a custom button interface overlaid on top of it. The main button is used to tell the Raspberry Pi to dispense a treat to the dog.
+
+![DogChat Interface](https://github.com/lauraskelton "DogChat Interface")
+
+![DogChat Notification](https://github.com/lauraskelton "DogChat Notification")
